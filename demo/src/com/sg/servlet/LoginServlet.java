2dbf6a62b86732fc016e77f2294127463576aec0
sgiceleo <sgiceleo@163.com>
2021-11-10 14:46:12 +0800

    first commit

---
+++ demo/src/com/sg/servlet/LoginServlet.java
@@ -0,0 +1,19 @@
+package com.sg.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("This is my first servlet!!");
+    }
+}
